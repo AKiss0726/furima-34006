a356a18e6a59a073ff6096a6f15d8080bbb32984
AKiss0726 <akihito.music.production@gmail.com>
2021-02-21 15:27:37 +0900

    商品削除機能修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -35,8 +35,12 @@ class ItemsController < ApplicationController
   end
 
   def destroy
+    if current_user.id == @item.user.id
     @item.destroy
-    redirect_to root_path(@item)
+    redirect_to root_path
+    else
+    redirect_to root_path
+    end
   end
 
   private
