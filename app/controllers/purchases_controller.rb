fd6a21c7d73aa910439c7da038cb42bc63cbc7e1
AKiss0726 <akihito.music.production@gmail.com>
2021-02-28 17:13:11 +0900

    クレジットカード決済機能実装、リファクタリング

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,6 +1,7 @@
 class PurchasesController < ApplicationController
-  before_action :set_purchase, only: [:index, :create]
-
+  before_action :authenticate_user!
+  before_action :set_purchase, only: [:index, :create, :move_to_index]
+  before_action :move_to_index
   def index
     @purchase_shipping = PurchaseShipping.new
   end
@@ -11,6 +12,7 @@ class PurchasesController < ApplicationController
   def create
     @purchase_shipping = PurchaseShipping.new(purchase_params)
     if @purchase_shipping.valid?
+      pay_item
       @purchase_shipping.save
       redirect_to root_path
     else
@@ -25,6 +27,21 @@ class PurchasesController < ApplicationController
 
   def set_purchase
     @item = Item.find(params[:item_id])
+  end
+
+  def pay_item
+    Payjp.api_key = "sk_test_1b5d484e4b6d6270f4c79ffa"
+      Payjp::Charge.create(
+        amount: @item.price,
+        card: purchase_params[:token],
+        currency: 'jpy'
+      )
+  end
+
+  def move_to_index
+    unless @item.user_id != current_user.id
+      redirect_to root_path
+    end
   end
 
 end
